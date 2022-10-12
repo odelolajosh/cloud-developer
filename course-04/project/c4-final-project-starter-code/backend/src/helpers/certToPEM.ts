3d78967a9548a1191b41462a02ed19016a2aa65f
odelolajosh <oladapoodelola@gmail.com>
2022-10-13 00:39:02 +0100

    feat: convert cert to pem

---
+++ course-04/project/c4-final-project-starter-code/backend/src/helpers/certToPEM.ts
@@ -0,0 +1,5 @@
+export function certToPEM(cert: string): string {
+  cert = cert.match(/.{1,64}/g).join('\n');
+  cert = `-----BEGIN CERTIFICATE-----\n${cert}\n-----END CERTIFICATE-----\n`;
+  return cert;
+}
